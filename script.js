8885c733d694e372b602be9af2ce3253ec7e0f78
MonteroSolisStephenDaniel <smontero25@ilb.cat>
2026-03-29 20:06:45 +0200

    variables globals i selecció d'elements HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let alumnes = [];
+
+let formulari = document.getElementById("formulariAlumne");
+let inputNom = document.getElementById("nom");
+let inputExamen = document.getElementById("examen");
+let inputPractiques = document.getElementById("practiques");
+let inputActitud = document.getElementById("actitud");
+let missatge = document.getElementById("missatge");
+let cosTaula = document.getElementById("cosTaula");
+let btnAsc = document.getElementById("ordenarAsc");
+let btnDesc = document.getElementById("ordenarDesc");
